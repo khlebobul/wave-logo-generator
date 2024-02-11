4de95621c83998d67ef3b1b4fc0fd6dbd63a4b0e
Gleb Shalimov <sbgleb10@gmail.com>
2024-02-11 18:11:51 +0300

    fixed icons and added inputs

--- script/icons.js
+++ script/icons.js
@@ -1,20 +1,22 @@
-let iconsData; // Перемещаем переменную в глобальную область видимости
+let iconsData; // Переместим переменную в глобальную область видимости
+let displayedIcons = []; // Переместим переменную в глобальную область видимости
 
 function displayIcons(iconsData) {
   const iconGrid = document.getElementById("iconGrid");
   iconGrid.innerHTML = '';
 
-  iconsData.forEach(function(iconEntry) {
-    const iconName = iconEntry;
+  displayedIcons = []; // Очищаем массив перед отображением иконок
 
-    const icon = document.createElement("i");
+  iconsData.forEach(function(iconEntry) {
+    const iconName = iconEntry[0]; // Получаем название иконки
+    const icon = document.createElement("div"); // Создаем элемент div для иконки
     icon.classList.add("icon");
-    icon.setAttribute("data-lucide", iconName);
-    icon.innerHTML = iconName;
+    icon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="${iconName}" class="lucide lucide-${iconName} icon"><rect width="18" height="18" x="3" y="3" rx="2"></rect><path d="M17 12h-2l-2 5-2 10-2 5H7"></path></svg>`;
     icon.onclick = function() {
       selectIcon(iconName);
     };
     iconGrid.appendChild(icon);
+    displayedIcons.push(icon);
   });
 }
 
@@ -30,8 +32,8 @@ function closeModal() {
 fetch('icons.json')
   .then(response => response.json())
   .then(data => {
-    iconsData = Object.keys(data); // Получаем только названия иконок
-    displayIcons(iconsData); // Отображение иконок после загрузки
+    iconsData = Object.entries(data); // Преобразуем объект в массив ключ-значение
+    displayIcons(iconsData); // Отображаем иконки после загрузки
   })
   .catch(error => {
     console.error('Ошибка загрузки иконок:', error);
@@ -39,14 +41,15 @@ fetch('icons.json')
 
 document.getElementById("searchIconInput").addEventListener("input", function() {
   const searchQuery = this.value.toLowerCase();
-  const filteredIconsData = iconsData.filter(function(iconName) {
-    return iconName.includes(searchQuery);
+  const filteredIconsData = iconsData.filter(function(iconEntry) {
+    const iconName = iconEntry[0]; // Получаем название иконки
+    return iconName.includes(searchQuery); // Возвращаем true, если название иконки содержит поисковой запрос
   });
 
-  displayIcons(filteredIconsData);
+  displayIcons(filteredIconsData); // Обновляем отображение иконок
 });
 
 function selectIcon(iconName) {
-  console.log("Selected icon:", iconName);
+  console.log("Выбрана иконка:", iconName);
   closeModal();
 }
